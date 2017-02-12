b78c29e94039213ed71242cfb8ca3093043e5185
GinMu <smu@anfengde.com>
2017-02-12 13:32:57 +0800

    add slot make  custom style and only save play logic

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,24 @@
       <label for="random">random</label>
     </div>
     <div class="audio-container">
-      <vue-audio v-for="(list, index) of lists" :source="list.source" :time="list.time" :index="index" :mode="mode"></vue-audio>
+      <vue-audio v-for="(list, index) of lists"
+                  :source="list.source"
+                  :index="index"
+                  :mode="mode"
+                  @timeupdate="timeupdate"
+                  @playing="playing"
+                  @pause="pause"
+                  @ended="ended"
+                  @waiting="waiting"
+                  @error="error">
+        <div class="player">
+          <svg class="progress" :width="svgOptions.width" :height="svgOptions.height">
+            <path :fill="svgOptions.fill" :stroke="svgOptions.stroke" :stroke-width="svgOptions.strokeWidth"></path>
+          </svg>
+          <a class="play" href="javascript:void(0)">
+          </a>
+        </div>
+      </vue-audio>
     </div>
   </div>
 </template>
@@ -46,7 +63,61 @@ export default {
       loop_mode: constant.LOOP,
       order_mode: constant.ORDER,
       circulation_mode: constant.CIRCULATION,
-      random_mode: constant.RANDOM
+      random_mode: constant.RANDOM,
+      svgOptions: {
+        width: 40,
+        height: 40,
+        fill: 'none',
+        stroke: '#007aff',
+        strokeWidth: 2
+      }
+    }
+  },
+  methods: {
+    timeupdate (e) {
+      let target = e.target
+      let currentTime = target.currentTime
+      let duration = target.duration
+      let percent = (100 / duration * currentTime).toFixed(1)
+      if (isNaN(percent) || percent === '0.0') {
+        percent = 0
+      }
+      let svg = target.parentNode.querySelector('svg')
+      if (percent > 100) {
+        percent = 100
+      }
+      let centerX = this.svgOptions.width / 2
+      let centerY = this.svgOptions.height / 2
+      let radius = centerX - this.svgOptions.strokeWidth / 2
+      let startX = centerX
+      let startY = centerY - radius
+      let xAxisRotation = 0
+      let sweepFlag = 1
+      let circ = Math.PI * 2
+      let largeArcFlag = 0
+      if (percent * circ / 100 >= Math.PI) {
+        largeArcFlag = 1
+      }
+      let endX = centerX + radius * Math.sin(circ * percent / 100)
+      let endY = centerY - radius * Math.cos(circ * percent / 100)
+      let d = 'M' + startX + ',' + startY + ' ' + 'A' + radius + ',' + radius + ' ' + xAxisRotation + ' ' + largeArcFlag + ' ' + sweepFlag + ' ' + endX + ',' + endY
+      let path = svg.querySelector('path')
+      path.setAttribute('d', d)
+    },
+    playing (e) {
+      e.path[1].querySelector('a').className = constant.PLAY_CLASS
+    },
+    pause (e) {
+      e.path[1].querySelector('a').className = constant.PAUSE_CLASS
+    },
+    ended (e) {
+      e.path[1].querySelector('a').className = constant.PAUSE_CLASS
+    },
+    waiting (e) {
+      e.path[1].querySelector('a').className = constant.LOAD_CLASS
+    },
+    error (e) {
+      console.log(e)
     }
   }
 }
@@ -66,3 +137,4 @@ export default {
   margin-bottom: 10px;
 }
 </style>
+<style lang="sass" src='./components/audio.scss' scoped>Error: Source sample is missing.</style>
--- src/components/audio.vue
+++ src/components/audio.vue
@@ -1,13 +1,19 @@
 <template lang="html">
-  <div class="player" @click.stop.prevent="startPlay">
-    <svg class="progress" :width="svgOptions.width" :height="svgOptions.height">
-      <path :fill="svgOptions.fill" :stroke="svgOptions.stroke" :stroke-width="svgOptions.strokeWidth" :d="d"></path>
-    </svg>
-    <a :class="currentState" href="javascript:void(0)">
-      <audio class="vue-audio" :muted="muted" :autoplay="autoplay" :preload="preload" :src="source" :loop="loop" @ended="_ended" @playing="_playing" @pause="_pause" @error="_error" @timeupdate="_timeupdate" @waiting="_waiting">
-      </audio>
-      <p class="time" v-text="currentProgress" v-if="time"></p>
-    </a>
+  <div class="vue-audio-container" @click.stop.prevent="startPlay">
+    <slot></slot>
+    <audio class="vue-audio"
+            :muted="muted"
+            :autoplay="autoplay"
+            :preload="preload"
+            :src="source"
+            :loop="loop"
+            @ended="_ended"
+            @playing="_playing"
+            @pause="_pause"
+            @error="_error"
+            @timeupdate="_timeupdate"
+            @waiting="_waiting">
+    </audio>
   </div>
 </template>
 
@@ -17,11 +23,7 @@ let currentTarget
 export default {
   data () {
     return {
-      currentTime: '',
-      currentState: constant.PAUSE_CLASS,
-      audios: [],
-      progress: '',
-      d: ''
+      audios: []
     }
   },
   props: {
@@ -31,10 +33,6 @@ export default {
       validator: function (value) {
         return value
       }
-    },
-    time: {
-      type: String,
-      default: ''
     },
     mode: {
       type: Number,
@@ -59,18 +57,6 @@ export default {
     muted: {
       type: Boolean,
       default: false
-    },
-    svgOptions: {
-      type: Object,
-      default () {
-        return {
-          width: 40,
-          height: 40,
-          fill: 'none',
-          stroke: '#007aff',
-          strokeWidth: 2
-        }
-      }
     }
   },
   mounted () {
@@ -81,9 +67,6 @@ export default {
   computed: {
     loop () {
       return this.mode === constant.LOOP
-    },
-    currentProgress () {
-      return this.progress || this.time
     }
   },
   methods: {
@@ -97,54 +80,26 @@ export default {
       }
     },
     _timeupdate (e) {
-      let target = e.target
-      let currentTime = target.currentTime
-      let duration = target.duration
-      let percent = (100 / duration * currentTime).toFixed(1)
-      if (isNaN(percent) || percent === '0.0') {
-        percent = 0
-      }
-      this.progress = percent === 0 ? '' : percent + '%'
-      if (percent > 100) {
-        percent = 100
-      }
-      let centerX = this.svgOptions.width / 2
-      let centerY = this.svgOptions.height / 2
-      let radius = centerX - this.svgOptions.strokeWidth / 2
-      let startX = centerX
-      let startY = centerY - radius
-      let xAxisRotation = 0
-      let sweepFlag = 1
-      let circ = Math.PI * 2
-      let largeArcFlag = 0
-      if (percent * circ / 100 >= Math.PI) {
-        largeArcFlag = 1
-      }
-      let endX = centerX + radius * Math.sin(circ * percent / 100)
-      let endY = centerY - radius * Math.cos(circ * percent / 100)
-      let d = 'M' + startX + ',' + startY + ' ' + 'A' + radius + ',' + radius + ' ' + xAxisRotation + ' ' + largeArcFlag + ' ' + sweepFlag + ' ' + endX + ',' + endY
-      this.d = d
+      this.$emit('timeupdate', e)
     },
     _playing (e) {
-      this.currentState = constant.PLAY_CLASS
-      this._stopOther(e.path[2])
+      this.$emit('playing', e)
+      this._stopOther(e.path[1])
     },
-    _pause () {
-      this.currentState = constant.PAUSE_CLASS
+    _pause (e) {
+      this.$emit('pause', e)
     },
-    _waiting () {
-      this.currentState = constant.LOAD_CLASS
+    _waiting (e) {
+      this.$emit('waiting', e)
     },
-    _ended () {
-      // it doesn't trigger end event when the mode is loop
-      this.progress = ''
-      this._pause()
-      this._typeControl()
+    _ended (e) {
+      this.$emit('ended', e)
+      this._modeControl()
     },
     _error (e) {
-      console.log(e)
+      this.$emit('error', e)
     },
-    _typeControl () {
+    _modeControl () {
       // single mode
       if (this.mode === constant.SINGLE) {
         return
@@ -190,5 +145,9 @@ export default {
   }
 }
 </script>
-
-<style lang="sass" src='./audio.scss' scoped>Error: Source sample is missing.</style>
+<style media="screen" lang="css" scoped>
+  .vue-audio-container {
+    position: relative;
+    display: inline-block;
+  }
+</style>
